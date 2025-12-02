d35560959f64815b01270fc10e73bc4d512f5705
nikhil p <110736999+117cps@users.noreply.github.com>
2025-12-02 20:29:17 +0000

    modified D2.py

--- D2/D2.py
+++ D2/D2.py
@@ -1,3 +1,20 @@
-with open("input.txt") as file:
+total = 0
+
+with open("sample.txt") as file:
     r = [i.split("-") for i in file.readline().rstrip().split(",")]
-print(r)
+ids = 0
+for i in r:
+    ids = [x for x in range(int(i[0]),int(i[1])+1)]
+    for i in ids:
+        numstr = str(i)
+        l = len(numstr)
+        if (len(numstr)%2)!=0:
+            continue
+
+        if numstr[0]*l==numstr:
+            total+=i
+            continue
+
+
+
+#print(r)
