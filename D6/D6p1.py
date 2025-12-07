6160470107e6f04529c8507157b335dea1f9f9c2
117cps <npp923@icloud.com>
2025-12-06 18:18:38 -0800

    D6!

--- D6/D6p1.py
+++ D6/D6p1.py
@@ -1,6 +1,6 @@
 import re
 
-FNAME = "sample"
+FNAME = "input"
 total = 0
 
 with open(f"D6/{FNAME}.txt") as file:
@@ -12,7 +12,18 @@ with open(f"D6/{FNAME}.txt") as file:
             except:
                 pass
 
-for i in range(0,len(li[0])):
-    if 
+for c in range(0,len(li[0])): 
+    if li[len(li)-1][c]=="+":
+        temp = 0
+        for r in range(0,len(li)-1):
+            temp+=li[r][c]
+    elif li[len(li)-1][c]=="*":
+        temp = 1
+        for r in range(0,len(li)-1):
+            temp*=li[r][c]
+    total+=temp
+    temp=0
 
-print(li)
+        
+
+print(total)
---
+++ D6/D6p2.py
@@ -0,0 +1,50 @@
+import re
+import copy
+
+FNAME = "input"
+total = 0
+
+with open(f"D6/{FNAME}.txt") as file:
+    li = [[i for i in x if i!="" and i!="\n"] for x in [line for line in file.readlines()]]
+
+temp=[]
+    
+for c in range(0,len(li[0])):
+    t=[]
+    for r in range(0,len(li)):
+        t.append(li[r][c])
+    temp.append(t)
+
+prl = []
+tn=[]
+opl = []
+
+for i in temp:
+    if all(t==" " for t in i):
+            prl.append(tn)
+            tn=[]
+            continue
+    n=""
+    for e in i:
+        if e not in " *+":
+            n+=e
+        elif e in "*+":
+             opl.append(e)
+    tn.append(n)
+prl.append(tn)    
+
+prl = [list(map(int, line)) for line in prl]
+
+for i in range(0,len(prl)):
+    if opl[i]=="*":
+        out = 1
+        for n in prl[i]:
+            out*=n
+        total+=out
+    elif opl[i]=="+":
+        out=0
+        for n in prl[i]:
+            out+=n
+        total+=out
+        
+print(total)
