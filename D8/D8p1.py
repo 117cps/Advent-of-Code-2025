df6fcd23c45fbac86fd4fd6f8f40acc736837f06
nikhil p <110736999+117cps@users.noreply.github.com>
2025-12-08 12:26:29 -0800

    D8 continued

--- D8/D8p1.py
+++ D8/D8p1.py
@@ -9,17 +9,32 @@ with open(f"D8/{FNAME}.txt") as file:
 
 dist = {}
 conn = {}
-
+connlist = []
 for p in inp:
     dist[p] = {}
     for i in inp:
         if i!=p:
             dist[p][i] = math.dist(p, i)
 
+for p in dist.keys():
+    s = min(dist[p], key=dist[p].get)
+    conn[p]=s
+    dist[p].pop(s)
+    dist[s].pop(p)
 
+for p in conn.keys():
+    connlist.append([p, conn[p]])
 
-test = (162, 817, 812)
-conn[test] = min(dist[test], key=dist[test].get)
-print(conn)
+for i in range(len(connlist)-1, -1, -1):
+    for x in range(len(connlist)-1, -1, -1):
+        if conn[i][0]!=0 and conn[x]!=0 and connlist[i][0] in connlist[x] and i!=x:
+            connlist[x].append(connlist[i][1])
+            connlist[i]=0
+            continue
+        elif conn[i][0]!=0 and conn[x]!=0 and connlist[i][1] in connlist[x] and i!=x:
+            connlist[x].append(connlist[i][0])
+            connlist[i]=0
+            continue
+print(connlist)
 
 #print(conn)
